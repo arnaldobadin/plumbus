3b2c1288674bdd9a59a8df8e1a680ddb1a96773b
Arnaldo Badin <arnaldobadin@gmail.com>
2019-08-15 13:55:55 -0300

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -2,116 +2,95 @@ const mysql = require("mysql");
 const fs = require("fs");
 
 const Mysql = function(host, user, password) {
-    if (!(host && typeof(host) == "string" && host.length)) throw new Error("Missing host on Mysql::Constructor.");
-    if (!(user && typeof(user) == "string" && user.length)) throw new Error("Missing user on Mysql::Constructor.");
-    if (!(password && typeof(password) == "string" && password.length)) throw new Error("Missing password on Mysql::Constructor.");
-    
-    this.config = {host, user, password, multipleStatements : true};
-    this.pool = null;
+	if (!(host && typeof(host) == "string" && host.length)) throw new Error("Missing host on Mysql::Constructor.");
+	if (!(user && typeof(user) == "string" && user.length)) throw new Error("Missing user on Mysql::Constructor.");
+	if (!(password && typeof(password) == "string" && password.length)) throw new Error("Missing password on Mysql::Constructor.");
 
-    this.status = false;
-}
+	this.config = {host, user, password, multipleStatements : true};
+	this.pool = null;
 
-Mysql.prototype.schema = function(path, callback) {
-    callback = callback || (() => {});
-    
-    if (this.status) return callback("Too late to create schema.", null);
-    if (!(path && typeof(path) == "string" && path.length)) {
-        return callback("Missing path.", null);
-    }
-
-    let schema = fs.readFileSync(path, "utf8");
-
-    if (!(schema && typeof(schema) == "string" && schema.length)) {
-        return callback("Missing schema.", null);
-    }
-
-    let connection = mysql.createConnection(this.config);
-    if (!connection) return callback("Missing connection.", null);
-
-    return connection.connect(
-        (error) => {
-            if (error) return callback(error || "Unknown error", null);
-            return connection.query(schema,
-                (error, result, fields) => {
-                    connection.end();
-                    if (error) return callback(error, null);
-                    return callback(null, result);
-                }
-            );
-        }
-    );
+	this.status = false;
 }
 
 Mysql.prototype.open = function(database, callback) {
-    callback = callback || (() => {});
+	callback = callback || (() => {});
 
-    if (this.status) return callback("Can't open another database.", null);
-    this.status = true;
+	if (this.status) return callback("Can't open another pool.", null);
+	this.status = true;
 
-    if (!(database && typeof(database) == "string" && database.length)) {
-        return callback("Invalid or missing database.", null);
-    }
+	if (!(database && typeof(database) == "string" && database.length)) {
+		return callback("Invalid or missing database.", null);
+	}
 
-    this.config.database = database;
-    this.config.connectionLimit = 99;
+	this.config.database = database;
+	this.config.connectionLimit = 99;
 
-    this.pool = mysql.createPool(this.config);
-    return callback(null, true);
+	this.pool = mysql.createPool(this.config);
+	return callback(null, true);
 }
 
-Mysql.prototype.close = function() {
-    if (!(this.status)) return false;
-    this.status = false;
+Mysql.prototype.close = function(callback) {
+	callback = callback || (() => {});
 
-    this.config = null;
-    this.pool.end();
-    return true;
+	if (!(this.status)) return callback("Can't close an empty pool.", null);;
+	this.status = false;
+
+	this.config = null;
+	this.pool.end();
+	return callback(null, true);
 }
 
-Mysql.prototype.insert = function(table, data, callback) {
-    callback = callback || (() => {});
-    if (!(table && data)) return callback("Missing data.", null);
+Mysql.prototype.schema = function(path, callback) {
+	callback = callback || (() => {});
 
-    let fields = [];
-    let values = [];
+	if (this.status) return callback("Too late to create schema.", null);
+	if (!(path && typeof(path) == "string" && path.length)) {
+		return callback("Missing path.", null);
+	}
 
-    for (let k in data) {
-        let row = data[k];
+	let schema = fs.readFileSync(path, "utf8");
 
-        if (!(k && row)) continue;
-        if (typeof(row) != "string" && typeof(row) != "number") continue;
-        if (typeof(row) == "string") row = "'" + row + "'";
+	if (!(schema && typeof(schema) == "string" && schema.length)) {
+		return callback("Missing schema.", null);
+	}
 
-        fields.push("`" + k + "`");
-        values.push(row);
-    }
+	let connection = mysql.createConnection(this.config);
+	if (!connection) return callback("Missing connection.", null);
 
-    fields = fields.join(",");
-    values = values.join(",");
-
-    const query = `INSERT INTO ${table} (${fields}) VALUES (${values});`;
-    return this.pool.query(query, callback);
+	return connection.connect(
+		(error) => {
+			if (error) return callback(error || "Unknown error", null);
+			return connection.query(schema,
+				(error, result, fields) => {
+					connection.end();
+					if (error) return callback(error, null);
+					return callback(null, result);
+				}
+			);
+		}
+	);
 }
 
 Mysql.prototype.query = function(query, callback) {
-    callback = callback || (() => {});
-    if (!query) return callback("Missing query.", null);
+	callback = callback || (() => {});
 
-    return this.pool.getConnection(
-        (error, connection) => {
-            if (error) return callback(error, null);
-            if (!connection) return callback("Missing connection.", null);
+	if (!this.status) return callback("Can't query an empty pool.", null);
+	if (!query) return callback("Missing query.", null);
 
-            return connection.query(query,
-                (error, result, fields) => {
-                    connection.release();
-                    if (error) return callback(error, null);
-                    return callback(null, result);
-                }
-            );
-        }
-    );
+	return this.pool.getConnection(
+		(error, connection) => {
+			if (error) return callback(error, null);
+			if (!connection) return callback("Missing connection.", null);
+
+			return connection.query(query,
+				(error, result, fields) => {
+					connection.release();
+					if (error) return callback(error, null);
+					return callback(null, result);
+				}
+			);
+		}
+	);
 }
 
 module.exports = Mysql;
