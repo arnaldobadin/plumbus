b496767681c337bf03bf3f101613640e79519916
arnaldobadin <arnaldobadin@gmail.com>
2019-09-12 19:03:13 -0300

    something

---
+++ examples/requiring.js
@@ -0,0 +1,4 @@
+const plumbus = require("../plumbus.js");
+
+const server = plumbus.server(1230);
+const types = plumbus.types();
--- plumbus.js
+++ plumbus.js
@@ -1,29 +1,35 @@
-const Actuator = require("./source/actuator.js");
-const Chest = require("./source/chest.js");
-const Logger = require("./source/logger.js");
-const Mysql = require("./source/mysql.js");
-const Queue = require("./source/queue.js");
-const Server = require("./source/server.js");
+const assets = {
+    classes : {
+        actuator : require("./source/actuator.js"),
+        chest : require("./source/chest.js"),
+        logger : require("./source/logger.js"),
+        mysql : require("./source/mysql.js"),
+        queue : require("./source/queue.js"),
+        server : require("./source/server.js")
+    },
+    objects : {
+        random : require("./source/random.js"),
+        sync : require("./source/sync.js"),
+        time : require("./source/time.js"),
+        token : require("./source/token.js"),
+        types : require("./source/types.js")
+    }
+};
 
-const Random = require("./source/random.js");
-const Sync = require("./source/sync.js");
-const Time = require("./source/time.js");
-const Token = require("./source/token.js");
-const Types = require("./source/types.js");
+const Plumbus = {};
 
-const Plumbus = {
-    actuator : (...params) => {return new Actuator(...params);},
-    chest : (...params) => {return new Chest(...params);},
-    logger : (...params) => {return new Logger(...params);},
-    mysql : (...params) => {return new Mysql(...params);},
-    queue : (...params) => {return new Queue(...params);},
-    server : (...params) => {return new Server(...params);},
+for (let k in assets.classes) {
+    let asset = assets.classes[k];
+    Plumbus[k] = (...params) => {
+        return new asset(...params);
+    }
+}
 
-    random : () => {return Random;},
-    sync : () => {return Sync;},
-    time : () => {return Time;},
-    token : () => {return Token;},
-    types : () => {return Types;}
+for (let k in assets.objects) {
+    let asset = assets.objects[k];
+    Plumbus[k] = () => {
+        return asset;
+    }
 }
 
 module.exports = Plumbus;
