83f6c433529a055270f49a473d0eac1ebb137d0f
Arnaldo Badin <arnaldobadin@gmail.com>
2019-09-17 02:47:11 -0300

    Create hash.js

---
+++ source/hash.js
@@ -0,0 +1,40 @@
+const crypto = require("crypto");
+
+const Hash = {};
+
+Hash.CHARACTER = {};
+Hash.CHARACTER.NORMAL = (
+    "QWERTYUIOPASDFGHJKLÇZXCVBNMçabcdefghijklmnopqrstuvwxyz1234567890!@#$%&*()_-+=;:><.,?{}]["
+);
+Hash.CHARACTER.SAFE = (
+    "QWERTYUIOPASDFGHJKLZXCVBNMabcdefghijklmnopqrstuvwxyz1234567890"
+);
+
+Hash.create = function(length, safe = false) {
+    if (safe) {
+        const token = Hash._generate(Hash.CHARACTER.SAFE, "sha512", length);
+        if (!token) return false;
+        return token.replace(/\+/gi, "-").replace(/\//gi, "_").replace(/=/gi, "o");
+    }
+    return Hash._generate(Hash.CHARACTER.NORMAL, "sha512", length);
+}
+
+Hash._generate = function(characters, code, length) {
+    if (!(characters && characters.length && code && code.length && length)) return false;
+
+    let uid = "";
+
+    for (let i = 0; i < length; i++) {
+        let index = Math.floor(Math.random() * characters.length);
+        uid += characters[index];
+    }
+    
+    if (!(uid && uid.length)) return false;
+    
+    const hash = crypto.createHash(code);
+    hash.update(uid, "utf-8");
+
+    return hash.digest("base64");
+}
+
+module.exports = Hash;
