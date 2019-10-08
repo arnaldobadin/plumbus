3460a27f5195cb1d83fdbef8dcb5550b9a5fe7f1
Arnaldo Badin <arnaldobadin@gmail.com>
2019-10-08 18:08:28 -0300

    Update time.js

--- source/time.js
+++ source/time.js
@@ -4,4 +4,8 @@ Time.epoch = function() {
 	return Math.floor(new Date() / 1000);
 }
 
+Time.now = function() {
+	new Date(new Date().toLocaleString(undefined, {hour12 : false}) + "Z");
+}
+
 module.exports = Time;
