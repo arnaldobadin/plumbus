dd31967724bb8bec5263df91174e062be4e8cb13
Arnaldo Badin <arnaldobadin@gmail.com>
2019-10-18 18:57:38 -0300

    Update sync.js

--- source/sync.js
+++ source/sync.js
@@ -24,4 +24,16 @@ Sync.result = function(mission, executor, ...params) {
 	);
 }
 
+Sync.wait = function(mission, executor, ...params) {
+	return new Promise(
+		(resolve, reject) => {
+			const operator = (error, result) => {
+				return resolve((error && new Error(error)) || result);
+			}
+			params.push(operator);
+			return mission.apply(executor, params);
+		}
+	);
+}
+
 module.exports = Sync;
