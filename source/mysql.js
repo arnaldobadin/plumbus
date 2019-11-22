7b65f58f169016daaa21515d026016dc36bd12e2
Arnaldo Badin <arnaldobadin@gmail.com>
2019-11-22 00:22:53 -0300

    Update mysql.js

--- source/mysql.js
+++ source/mysql.js
@@ -1,5 +1,4 @@
 const mysql = require("mysql");
-const fs = require("fs");
 
 const Mysql = function(host, user, password) {
 	if (!(host && typeof(host) == "string" && host.length)) throw new Error("Missing host on Mysql::Constructor.");
@@ -21,28 +20,28 @@ Mysql.prototype.connected = function() {
 }
 
 Mysql.prototype.open = async function(database) {
-	if (this._status) return new Error(`Can't open another pool.`);
+	if (this._status) throw new Error(`Can't open another pool.`);
 	this._status = true;
 
 	if (!(database && typeof(database) == "string" && database.length)) {
-		return new Error(`Invalid or missing database.`);
+		throw new Error(`Invalid or missing database.`);
 	}
 
 	this._config.database = database;
 	this._config.connectionLimit = 99;
 
 	this._pool = mysql.createPool(this._config);
-	if (!this._pool) return new Error(`Failed on creating pool.`);
+	if (!this._pool) throw new Error(`Failed on creating pool.`);
 
 	const status = await this.query(Mysql.QUERY.STATUS);
-	if (status instanceof Error) return new Error(`Pool failed on connect: ${status}`);
+	if (status instanceof Error) throw new Error(`Pool failed on connect: '${status}'`);
 
 	this._connected = true;
 	return `Pool created with success.`;
 }
 
 Mysql.prototype.close = async function() {
-	if (!(this._status)) return new Error(`Can't close an empty pool.`);
+	if (!(this._status)) throw new Error(`Can't close an empty pool.`);
 	this._status = false;
 
 	this._config = null;
@@ -53,21 +52,21 @@ Mysql.prototype.close = async function() {
 }
 
 Mysql.prototype.query = async function(query) {
-	if (!this._status) return new Error(`Can't query an empty pool.`);
+	if (!this._status) throw new Error(`Can't query an empty pool.`);
 	if (!(query && typeof(query) == "string" && query.length)) {
-		return new Error(`Missing query.`);
+		throw new Error(`Missing query.`);
 	}
 	
 	return await new Promise((resolve, reject) => {
         return this._pool.getConnection((error, connection) => {
-            if (error) return resolve(new Error(error));
-            if (!connection) return resolve(
+            if (error) return reject(new Error(error));
+            if (!connection) return reject(
                 new Error(`Can't get a valid connection.`)
             );
 
             return connection.query(query, (error, result, fields) => {
                 connection.release();
-                if (error) return resolve(new Error(error));
+                if (error) return reject(new Error(error));
                 return resolve(result);
             });
         });
