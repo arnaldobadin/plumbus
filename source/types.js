b689254603868f18686bd6e931ec2a3bd199c158
Arnaldo Badin <arnaldobadin@gmail.com>
2020-02-12 15:30:37 -0300

    Update types.js

--- source/types.js
+++ source/types.js
@@ -1,57 +1,57 @@
 const Types = {};
 
 Types.DATATYPE = {
-	NUMBER : "number", STRING : "string", OBJECT : "object",
-    	ARRAY : "array", FUNCTION : "function", ERROR : "error",
-    	SYMBOL : "symbol"
+    NUMBER : "number", STRING : "string", OBJECT : "object",
+    ARRAY : "array", FUNCTION : "function", ERROR : "error",
+    SYMBOL : "symbol"
 };
 
 Types.string = function(value) {
-	return typeof(value) === Types.DATATYPE.STRING || value instanceof String;
+    return typeof(value) === Types.DATATYPE.STRING || value instanceof String;
 }
 
 Types.number = function(value) {
-	return typeof(value) === Types.DATATYPE.NUMBER && Number.isFinite(value);
+    return typeof(value) === Types.DATATYPE.NUMBER && Number.isFinite(value);
 }
 
 Types.bool = function(value) {
-	return Types.number(value) && (value === 0 || value === 1);
+    return Types.number(value) && (value === 0 || value === 1);
 }
 
 Types.integer = function(value) {
-	return Types.number(value) && Number.isInteger(value);
+    return Types.number(value) && Number.isInteger(value);
 }
 
 Types.float = function(value) {
-	return Types.number(value) && ((value % 1) !== 0);
+    return Types.number(value) && ((value % 1) !== 0);
 }
 
 Types.port = function(value) {
-	return Types.number(value) && (value >= 0 && value <= 65535);
+    return Types.number(value) && (value >= 0 && value <= 65535);
 }
 
 Types.array = function(value) {
-	return value != null && typeof(value) === Types.DATATYPE.OBJECT && value.constructor === Array;
+    return value != null && typeof(value) === Types.DATATYPE.OBJECT && value.constructor === Array;
 }
 
 Types.object = function(value) {
-	return value != null && typeof(value) === Types.DATATYPE.OBJECT && value.constructor === Object;
+    return value != null && typeof(value) === Types.DATATYPE.OBJECT && value.constructor === Object;
 }
 
 Types.function = function(value) {
-	return typeof(value) === Types.DATATYPE.FUNCTION;
+    return typeof(value) === Types.DATATYPE.FUNCTION;
 }
 
 Types.symbol = function(value) {
-	return typeof(value) == Types.DATATYPE.SYMBOL;
+    return typeof(value) == Types.DATATYPE.SYMBOL;
 }
 
 Types.error = function(value) {
-	return value instanceof Error;
+    return value instanceof Error;
 }
 
 Types.date = function(value) {
-	return value instanceof Date;
+    return value instanceof Date;
 }
 
 Types.instance = function(value, type) {
